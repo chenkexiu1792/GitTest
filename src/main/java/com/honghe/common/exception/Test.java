8fa8aab2da707c03b83e231626c97c19e61f9d47
chenkexiu1792 <chenkexiu1792@163.com>
2024-11-19 22:33:08 +0800

    master add d

--- src/main/java/com/honghe/common/exception/Test.java
+++ src/main/java/com/honghe/common/exception/Test.java
@@ -4,21 +4,7 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("master add one line content");
-        System.out.println(" dev1 add one line content");
-        System.out.println(" dev1 add one line content");
-        System.out.println(" dev1 add one line content");
-        System.out.println(" dev1 add one line content");
-        System.out.println(" dev1 add one line content");
-        System.out.println(" dev1 a ");
-        System.out.println(" dev1 b ");
-        System.out.println(" dev1 c ");
-        System.out.println(" dev1 e ");
-        System.out.println(" dev1 f ");
-
-        System.out.println(" feature  add a ");
-        System.out.println(" feature  add b");
-        System.out.println(" feature  add c ");
+        System.out.println(" master add one line content");
         System.out.println(" master  add d ");
     }
 }
