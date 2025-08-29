df3342d51922025ed6f72d649e1267aa409a5188
chenkexiu1792 <chenkexiu1792@163.com>
2025-08-29 14:01:28 +0800

    branch master add Master6.java

---
+++ src/main/java/com/honghe/common/exception/Master6.java
@@ -0,0 +1,8 @@
+package com.honghe.common.exception;
+
+public class Master6 {
+
+    public static void main(String[] args) {
+        System.out.println("branch master add Master6.java");
+    }
+}
