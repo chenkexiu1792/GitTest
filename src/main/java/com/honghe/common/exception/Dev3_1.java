787c8ecae7f651bab87e9660548181a6fce32f35
chenkexiu1792 <chenkexiu1792@163.com>
2025-08-29 11:38:00 +0800

    branch dev2  Dev2_1 cherry pick test 20250829

--- src/main/java/com/honghe/common/exception/Dev3_1.java
+++ src/main/java/com/honghe/common/exception/Dev3_1.java
@@ -3,6 +3,8 @@ package com.honghe.common.exception;
 public class Dev3_1 {
 
     public static void main(String[] args) {
-        System.out.println("Dev3_1");
+        System.out.println("Dev2_1");
+
+        System.out.println(" Dev2_1 cherry pick test 20250829");
     }
 }
