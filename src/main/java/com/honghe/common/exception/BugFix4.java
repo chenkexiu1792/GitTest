9d8e2daf28c5c818b5fc6da10175a1daa7b1e3e4
chenkexiu1792 <chenkexiu1792@163.com>
2025-10-30 11:52:09 +0800

    branch bugfix4 add  BugFix4.java

---
+++ src/main/java/com/honghe/common/exception/BugFix4.java
@@ -0,0 +1,10 @@
+package com.honghe.common.exception;
+
+public class BugFix4 {
+
+    public static void main(String[] args) {
+        System.out.println("bugFix3");
+        System.out.println("bugFix4");
+        System.out.println("bugFix5");
+    }
+}
