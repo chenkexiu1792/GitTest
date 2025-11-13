74f9575301bf3a026c8600b8cf304e0ce9eb7998
chenkexiu1792 <chenkexiu1792@163.com>
2025-11-13 14:29:39 +0800

    branch dev2 update Test6

--- src/main/java/com/honghe/common/exception/Test6.java
+++ src/main/java/com/honghe/common/exception/Test6.java
@@ -3,5 +3,6 @@ package com.honghe.common.exception;
 public class Test6 {
     public static void main(String[] args) {
         System.out.println("branch dev2 add Test6.java");
+        System.out.println("branch dev2 add Test6.java");
     }
 }
